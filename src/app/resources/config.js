aefa4eacd4f477e07ccc4983d56c8f302c4fa7da
Shafiq Ninaba <79973831+shafiqninaba@users.noreply.github.com>
2025-05-09 09:51:10 +0800

    Update baseURL in config.js for SEO

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,5 +1,5 @@
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL = "https://demo.magic-portfolio.com";
+const baseURL = "https://shafiqninaba.vercel.app/";
 
 const routes = {
   "/": true,
