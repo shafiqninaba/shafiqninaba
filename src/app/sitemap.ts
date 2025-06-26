9d96c551c7fd6109c07452af83f83a40a5326dbe
Shafiq Ninaba <shafiqninaba@gmail.com>
2025-06-26 14:10:57 +0800

    fix: comment out blog posts retrieval in sitemap function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,10 +2,10 @@ import { getPosts } from "@/app/utils/utils";
 import { baseURL, routes as routesConfig } from "@/app/resources";
 
 export default async function sitemap() {
-  const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
-    url: `${baseURL}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  // const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
+  //   url: `${baseURL}/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }));
 
   const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
     url: `${baseURL}/work/${post.slug}`,
@@ -19,5 +19,6 @@ export default async function sitemap() {
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs, ...works];
+  // return [...routes, ...blogs, ...works];
+  return [...routes, ...works];
 }
